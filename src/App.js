5e7a0f6be71570f4db75745970db512900efb77f
Christine Jeon <csjeon28@gmail.com>
2021-06-08 12:54:53 -0400

    commit tree 176bbfa04248b5d62004ca22766945cc29462467

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { Component } from 'react'
+
+const api_url = 'http://api.open-notify.org/astros.json';
+class App extends Component {
+
+    state = {
+        peopleInSpaceList: []
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.peopleInSpaceList.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch(api_url)
+            .then(resp => resp.json())
+            .then(({ people }) => this.setState({ peopleInSpaceList: people }))
+    }
+}
+
+export default App;
